c98ac15d7b3d049352248cac2b53f8c3acef7451
petraalpigiano <petraalpigiano@gmail.com>
2025-05-21 18:41:00 +0200

    add footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,84 @@ export default function Footer() {
   return (
     <>
       <section className="background-links">
-        <div className="container content-footer"> </div>
+        <div className="container content-links">
+          <div className="row r-footer">
+            <div class="col c-footer">
+              <h5>DC COMICS </h5>
+              <nav className="nav-footer">
+                <a className="footer-link" href="#">
+                  Characters
+                </a>
+                <a className="footer-link" href="#">
+                  Comics
+                </a>
+                <a className="footer-link" href="#">
+                  Shop DC
+                </a>
+                <a className="footer-link" href="#">
+                  Terms of Use
+                </a>
+                <a className="footer-link" href="#">
+                  DC kids
+                </a>
+              </nav>
+            </div>
+            <div class="col c-footer">
+              <h5>DC</h5>
+              <nav className="nav-footer">
+                <a className="footer-link" href="#">
+                  Characters
+                </a>
+                <a className="footer-link" href="#">
+                  Comics
+                </a>
+                <a className="footer-link" href="#">
+                  Shop DC
+                </a>
+                <a className="footer-link" href="#">
+                  Terms of Use
+                </a>
+                <a className="footer-link" href="#">
+                  DC kids
+                </a>
+              </nav>
+            </div>
+            <div class="col c-footer">
+              <h5>SITES</h5>
+              <nav className="nav-footer">
+                <a className="footer-link" href="#">
+                  Characters
+                </a>
+                <a className="footer-link" href="#">
+                  Comics
+                </a>
+                <a className="footer-link" href="#">
+                  Shop DC
+                </a>
+                <a className="footer-link" href="#">
+                  Terms of Use
+                </a>
+                <a className="footer-link" href="#">
+                  DC kids
+                </a>
+              </nav>
+            </div>
+          </div>
+          <img src="/p_img/dc-logo-bg.png"></img>
+        </div>
       </section>
       <section className="background-socials">
-        <div className="container content-footer"> </div>
+        <div className="container content-socials">
+          <button>SIGN UP NOW!</button>
+          <div className="socials">
+            <h4>FOLLOW US</h4>
+            <img src="/p_img/footer-facebook.png"></img>
+            <img src="/p_img/footer-twitter.png"></img>
+            <img src="/p_img/footer-youtube.png"></img>
+            <img src="/p_img/footer-pinterest.png"></img>
+            <img src="/p_img/footer-periscope.png"></img>
+          </div>
+        </div>
       </section>
     </>
   );
