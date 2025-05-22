99e58a0beba37e07fe95da05d3512ba108344bd3
petraalpigiano <petraalpigiano@gmail.com>
2025-05-22 17:56:36 +0200

    add base section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,6 +3,9 @@ import ComicList from "./ComicList";
 export default function Main() {
   return (
     <>
+      <div className="background-teenTitans">
+        <div className="container current-series">CURRENT SERIES</div>
+      </div>
       <div className="background-main">
         <ComicList />
         <div className="container content-button">
